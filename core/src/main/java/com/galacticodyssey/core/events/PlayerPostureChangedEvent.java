6b3ff725c6dfecf93eba0c508b070ea72acc7b2e
Bubba <jamesfargoth2@gmail.com>
2026-05-28 23:56:30 -0500

    style(player): make PlayerPostureChangedEvent final

--- core/src/main/java/com/galacticodyssey/core/events/PlayerPostureChangedEvent.java
+++ core/src/main/java/com/galacticodyssey/core/events/PlayerPostureChangedEvent.java
@@ -2,7 +2,7 @@ package com.galacticodyssey.core.events;
 
 import com.galacticodyssey.player.PostureType;
 
-public class PlayerPostureChangedEvent {
+public final class PlayerPostureChangedEvent {
     public final PostureType previous;
     public final PostureType next;
     public PlayerPostureChangedEvent(PostureType previous, PostureType next) {
